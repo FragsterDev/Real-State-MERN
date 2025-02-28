7cb15bc119438f9c968c18414f4776db3ee6dd57
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-02-28 21:28:46 +0530

    used react-router instead of react-router-dom

--- Real-Estate-MERN/src/routes/RoutePaths.tsx
+++ Real-Estate-MERN/src/routes/RoutePaths.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Profile from "../pages/Profile";
