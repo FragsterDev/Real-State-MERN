5a870d2a58ba05977dd6fed00c3ae93c314f68c6
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-02-28 21:38:59 +0530

    header component added...wip...

--- Real-Estate-MERN/src/routes/RoutePaths.tsx
+++ Real-Estate-MERN/src/routes/RoutePaths.tsx
@@ -4,9 +4,11 @@ import About from "../pages/About";
 import Profile from "../pages/Profile";
 import SignIn from "../pages/SignIn";
 import SignOut from "../pages/SignOut";
+import Header from "../components/header";
 
 function RoutePaths() {
   return <BrowserRouter>
+    <Header/>
     <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
