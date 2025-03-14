741cd040723552a9da98a23f4274384fbfcc99eb
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-03-14 21:39:30 +0530

    routes added to App.tsx file

--- Real-Estate-MERN/src/App.tsx
+++ Real-Estate-MERN/src/App.tsx
@@ -1,8 +1,23 @@
-import RoutePaths from "./routes/RoutePaths";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <RoutePaths />
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+        <Route path="/signout" element={<SignOut/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
