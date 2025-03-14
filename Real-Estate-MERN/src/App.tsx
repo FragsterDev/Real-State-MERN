b7e7fdc4b0a0c993ccbbebfc14ddef3fe3e4be52
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-03-14 22:43:01 +0530

    lazy loading added

--- Real-Estate-MERN/src/App.tsx
+++ Real-Estate-MERN/src/App.tsx
@@ -1,22 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
+import { lazy, Suspense } from "react";
 import Header from "./components/Header";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Profile = lazy(() => import("./pages/Profile"));
+const SignIn = lazy(() => import("./pages/SignIn"));
+const SignOut = lazy(() => import("./pages/SignOut"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/signin" element={<SignIn/>}/>
-        <Route path="/signout" element={<SignOut/>}/>
-      </Routes>
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route>
+            <Route index element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/signin" element={<SignIn/>}/>
+            <Route path="/signout" element={<SignOut/>}/>
+          </Route>
+          
+          <Route path="*" element={<p className="text-2xl text-center">404 Not Found</p>}/>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
