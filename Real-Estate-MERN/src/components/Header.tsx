49bad486d92a975f6023c2b5bb32e7b77f25997a
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-03-15 15:52:15 +0530

    header done

--- Real-Estate-MERN/src/components/Header.tsx
+++ Real-Estate-MERN/src/components/Header.tsx
@@ -4,51 +4,52 @@ import { Link } from "react-router-dom"
 function Header() {
   return (
     <header
-      className="flex h-[68px] bg-slate-200 shadow-md justify-around items-center"
+      className="bg-slate-200 shadow-md"
     >
-      <Link to={"/"}>
-
-        <div
-          onClick={() => {}}
-          className="font-bold text-sm sm:text-xl flex flex-wrap hover:cursor-pointer"
-        >
-          <span className="text-slate-500">Estate</span>
-          <span className="text-slate-700">Siliguri</span>
-        </div>
-      </Link>
-
-      <form 
-        action=""
-        className="flex bg-slate-100 items-center p-3 rounded-lg"
-      >
-        <input 
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search..." 
-          className="w-24 sm:w-64 focus:outline-none"
-        />
-        <button
-          type="submit"
-          className="hover:cursor-pointer"
-        >
-          <FaSearch className="text-slate-500"/>
-        </button>
-      </form>
-
-      <ul className="flex items-center gap-4">
+      <div className="flex max-w-6xl mx-auto justify-between items-center p-3">
         <Link to={"/"}>
-          <li className="hover:cursor-pointer">Home</li>
+          <div
+            onClick={() => {}}
+            className="font-bold text-sm sm:text-xl flex flex-wrap gap-1 hover:cursor-pointer"
+          >
+            <span className="text-slate-500">Estate</span>
+            <span className="text-slate-700">Siliguri</span>
+          </div>
         </Link>
 
-        <Link to={"/about"}>
-          <li className="hover:cursor-pointer">About</li>
-        </Link>
+        <form 
+          action=""
+          className="flex bg-slate-100 items-center p-3 rounded-lg"
+        >
+          <input 
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search..." 
+            className="w-24 sm:w-64 focus:outline-none text-xs sm:text-sm"
+          />
+          <button
+            type="submit"
+            className="hover:cursor-pointer"
+          >
+            <FaSearch className="text-slate-500"/>
+          </button>
+        </form>
 
-        <Link to={"/signin"}>
-          <li className="hover:cursor-pointer">Sign In</li>
-        </Link>
-      </ul>
+        <ul className="flex items-center gap-4">
+          <Link to={"/"}>
+            <li className="hover:cursor-pointer">Home</li>
+          </Link>
+
+          <Link to={"/about"}>
+            <li className="hover:cursor-pointer">About</li>
+          </Link>
+
+          <Link to={"/signin"}>
+            <li className="hover:cursor-pointer">Sign In</li>
+          </Link>
+        </ul>
+      </div>
     </header>
   )
 }
