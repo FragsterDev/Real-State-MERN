05ce88644423a44624efcb69ee51ddec72215ad3
Fragster Dev <pratiksharma059@gmail.com>
2025-03-28 21:34:30 +0530

    formality lol

--- api/app.js
+++ api/app.js
@@ -1,4 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongouri = process.env.MONGOOSE;
+
+if(!mongouri){
+    console.log('Mongo URI not found !');
+    process.exit(1);
+}
+
+const db = mongoose.connect(mongouri)
+.then(() => {
+    console.log('DB connection successful');
+})
+.catch((err) => {
+    console.log(err);
+});
 
 const port = 3000
 const app = express();
