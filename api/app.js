254f534f2f63782d87230f5f96897231054f8039
Pratik <pratiksharma@Pratiks-Mac-mini-2.local>
2025-03-28 15:58:11 +0530

    mongo db connection made

--- api/app.js
+++ api/app.js
@@ -1,9 +1,28 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const port = 3000
+dotenv.config();
+
+const mongo_uri = process.env.MONGO_URI;
+
+if (!mongo_uri) {
+  console.log("Invalid mongo uri");
+  process.exit(1);
+}
+
+const db = mongoose
+  .connect(mongo_uri)
+  .then(() => {
+    console.log("Successful Connection");
+  })
+  .catch((err) => {
+    console.log("Error:", err);
+  });
+
+const port = 3000;
 const app = express();
 
 app.listen(port, () => {
-    console.log(`Server running at ${port}`);
+  console.log(`Server running at ${port}`);
 });
-
